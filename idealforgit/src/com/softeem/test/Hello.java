fc85154cfa8c6ac3c46fea914f6724e19bd72488
xiaotao <1540287655@qq.com>
2023-03-17 15:06:21 +0800

    我对hello.java类文件进行了第一次修改，增加了两行代码。

--- idealforgit/src/com/softeem/test/Hello.java
+++ idealforgit/src/com/softeem/test/Hello.java
@@ -2,6 +2,8 @@ package com.softeem.test;
 
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("我也是新加的代码！");
         System.out.println("测试一下！");
+        System.out.println("我又添加了一句代码！");
     }
 }
